b99e5b9f6a1b2b5e5e5931ccf14380c14a133806
defrak2 <defranciscok22@gmail.com>
2024-05-28 19:45:53 -0400

    handleAddTask function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-const inputDate = $('#inputDate');
-const inputTitle = $('#inputTitle');
-const inputDescription = $('#inputDescription');
+
 
 
 // Todo: create a function to generate a unique task id
@@ -15,8 +13,20 @@ function generateTaskId(event) {
 // Todo: create a function to create a task card
 function createTaskCard(task) {
 
-
+  // const taskCard = $('<div>')
+  //   .addClass('card project-card draggable my-3')
+  //   .attr('data-task-id', task.id);
+  //   const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
+  //   const cardBody = $('<div>').addClass('card-body');
+  //   const cardDescription = $('<p>').addClass('card-text').text(task.description);
+  //   const cardDueDate = $('<p>').addClass('card-text').text(project.dueDate);
+  //   const cardDeleteBtn = $('<button>')
+  //     .addClass('btn btn-danger delete')
+  //     .text('Delete')
+  //     .attr('data-task-id', task.id);
+  // cardDeleteBtn.on('click', handleDeleteTask)  
 }
+
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -26,22 +36,28 @@ function renderTaskList() {
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
   event.preventDefault();
-  const taskDate = inputDate.val();
-  const taskTitle = inputTitle.val().trim();
-  const taskDescription = inputDescription.val();
-  const taskStatus = 'to-do'
+  const taskTitle = $('#inputTitle').val().trim();  
+  const taskDate = $('#inputDate').val();
+  const taskDescription = $('#inputDescription').val();
+
  
   const newTask = {
    id: generateTaskId(),
    title: taskTitle,
    date: taskDate,
    description: taskDescription,
-   status: taskStatus
-  }
+   status: 'to-do'
+  };
+
+  const tasks = readTasksFromStorage();
  
-  taskList.push(newTask);
+  tasks.push(newTask);
 
+  saveTasksToStorage(tasks);
 
+  $('#formModal').modal('hide');
+
+  renderTaskList(tasks);
 }
 
 // Todo: create a function to handle deleting a task
@@ -68,3 +84,4 @@ $( function() {
 
 
 
+// focus on handle add task first. create the object for the modal pop up input. get that saved to local storage. pass that array to create task card. make cards, here you can add the color functionality for warning and late. push that card to a new array lastly return that array and pass it to render task list. where you place the cards based off the ids #todo #inprogress and #done
