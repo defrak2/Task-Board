0905052c2015092a4e69f45191ef7463ec0cd3ee
defrak2 <defranciscok22@gmail.com>
2024-05-29 11:02:39 -0400

    fixed bug in rendering tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+// let taskCardEl = $('.task-card')
 
 //Created funtion to read tasks from Local Storage
 function readTasksFromStorage() {
@@ -29,28 +30,27 @@ function generateTaskId() {
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-
+function createTaskCard(tasks) {
   const taskCard = $('<div>')
-    .addClass('card project-card draggable my-3')
-    .attr('data-task-id', task.id);
-    const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
+    .addClass('card task-card draggable my-3')
+    .attr('data-task-id', tasks.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(tasks.title);
     const cardBody = $('<div>').addClass('card-body');
-    const cardDescription = $('<p>').addClass('card-text').text(task.description);
-    const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+    const cardDescription = $('<p>').addClass('card-text').text(tasks.description);
+    const cardDueDate = $('<p>').addClass('card-text').text(tasks.date);
     const cardDeleteBtn = $('<button>')
       .addClass('btn btn-danger delete')
       .text('Delete')
-      .attr('data-task-id', task.id);
-  cardDeleteBtn.on('click', handleDeleteTask)  
+      .attr('data-task-id', tasks.id);
+  cardDeleteBtn.on('click', handleDeleteTask);  
 
-  if(task.taskDate && task.status !== 'done') {
+  if(tasks.date && tasks.status !== 'done') {
     const now = dayjs();
-    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYY');
+    const taskDate = dayjs(tasks.date, 'DD/MM/YYY');
 
-  if (now.isSame(taskDueDate, 'day')) {
+  if (now.isSame(taskDate, 'day')) {
     taskCard.addClass('bg-warning text-white');
-  } else if (nowisAfter(taskDueDate)) {
+  } else if (now.isAfter(taskDate)) {
     taskCard.addClass('bg-danger text-white');
     cardDeleteBtn.addClass('border-light');
   }
@@ -83,21 +83,21 @@ for (let task of tasks) {
   }else if (task.status === 'in-progress') {
     inProgressList.append(createTaskCard(task))
   } else if (task.status === 'done') {
-    doneList.append(createTaskCard(project))
+    doneList.append(createTaskCard(task))
   }
 }
 
-$('draggable').draggable({
+$('.draggable').draggable({
   opacity: .7,
   zIndex: 100,
   helper: function (e) {
-    const original = $(e.target.hasClass ('ui-draggable'))
+    const original = $(e.target).hasClass('ui-draggable')
     $(e.target)
     $(e.target).closest('.ui-draggable');
 
-    return original.clone().css({
-      width: original.outerWidth(),
-    });
+    // return original.clone().css({
+    //   width: original.outerWidth(),
+    // });
   },
 });
 }
@@ -131,16 +131,44 @@ function handleAddTask(event){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
+  const taskId = $(this).attr('data-task-id');
+  const tasks = readTasksFromStorage();
 
+  tasks.forEach((task) => {
+    if (task.id === taskId) {
+      tasks.splice(tasks.indexOf(task), 1);
+    }
+  });
+  saveTasksToStorage(tasks);
+
+  renderTaskList();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+  const tasks = readTasksFromStorage();
 
+  const taskId = ui.draggable[0].dataset.taskId;
+ 
+  const newStatus = event.target.id;
+
+  for (let task of tasks) {
+    if (task.id === taskId) {
+      task.status = newStatus;
+    }
+  }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  renderTaskList();
+
+  $('.lane').droppable({
+    accept: '.draggable',
+    drop: handleDrop,
+  });
 
 
 $( function() {
@@ -149,6 +177,8 @@ $( function() {
 
 $('#saveButton').on('click', handleAddTask);
 });
+
+// taskCard.on('click', '.cardDeleteBtn', handleDeleteTask);
 
 
 
