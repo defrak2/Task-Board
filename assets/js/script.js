1d0a18dea56f8dcd0f31f6dc7ff5ae4f5740c48b
defrak2 <defranciscok22@gmail.com>
2024-05-29 11:53:42 -0400

    fixed droppable functionalities bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,7 +168,7 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
   renderTaskList();
 
-  $('.card-body').droppable({
+  $('.lane').droppable({
     accept: '.draggable',
     drop: handleDrop,
   });
@@ -187,3 +187,6 @@ $('#saveButton').on('click', handleAddTask);
 
 
 // focus on handle add task first. create the object for the modal pop up input. get that saved to local storage. pass that array to create task card. make cards, here you can add the color functionality for warning and late. push that card to a new array lastly return that array and pass it to render task list. where you place the cards based off the ids #todo #inprogress and #done
+
+
+//Whenever dragging the 
